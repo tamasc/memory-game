d62da3e6110f24cc534c6a995a7cd6acfebd4726
tamasc <tamas.czerjak@gmail.com>
2020-06-01 20:06:35 +0200

    basic app with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HighScoresComponent } from './high-scores/high-scores.component';
+import { GameComponent } from './game/game.component';
+import { NewGameComponent } from './new-game/new-game.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'highscores', component: HighScoresComponent },
+  { path: 'new-game', component: NewGameComponent },
+  { path: 'game', component: GameComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HighScoresComponent } from './high-scores/high-scores.component';
+import { GameComponent } from './game/game.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NewGameComponent } from './new-game/new-game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HighScoresComponent,
+    GameComponent,
+    NavBarComponent,
+    NewGameComponent,
   ],
   imports: [
     BrowserModule,
