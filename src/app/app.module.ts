08cab9163c44ea6a32048ffe10f1a8244c626a18
tamasc <tamas.czerjak@gmail.com>
2020-06-03 23:48:36 +0200

    add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
 import { HighScoresComponent } from './high-scores/high-scores.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { NewGameComponent } from './new-game/new-game.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -23,6 +29,17 @@ import { NewGameComponent } from './new-game/new-game.component';
 	AppRoutingModule,
 	HttpClientModule,
 	FormsModule,
+	BrowserAnimationsModule,
+	MatButtonModule,
+	MatInputModule,
+	MatDialogModule,
+	MatToolbarModule,
+  ],
+  exports: [
+	MatButtonModule,
+	MatInputModule,
+	MatDialogModule,
+	MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
